72630831670d06a567c73af5d626dc727bc1151b
Alexandrelinodasilva <alexandre.lino.silva@escola.pr.gov.br>
2025-09-22 10:48:46 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -4,10 +4,23 @@ function MostraPalavrasChave(){
 const texto = document.querySelector("#entrada-de-texto").value;
 const campoResultado = document.querySelector("#resultado-palavrachave");
 campoResultado.textContent = texto;
-const palavrachave = processaTexto(texto);
+const palavrasChave = processaTexto(texto);
 campoResultado.textContent = palavras.join(" , ");
 }
 function processaTexto(texto){
     let palavras = texto.split(/\P{L}+/u);
+
+    let frequencias = {};
+    for (let i of palavras){
+        frequencias [i] = 0;
+   
+        for (let j of palavras){
+        if (i == j){
+            frequencias[i]++;
+        }
+    }
+    }
+console.log(frequencias);
     return palavras;
 }
+
